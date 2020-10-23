ed483766cd4043f15048f6a63d1191763fcca402
alexandradilja <alexandrab18@ru.is>
2020-10-23 12:31:37 +0000

    order service done

--- order_service/data/db.js
+++ order_service/data/db.js
@@ -2,8 +2,9 @@ const orderSchema = require('./schema/order');
 const orderItemSchema = require('./schema/orderItem');
 const mongoose = require('mongoose');
 
-const connection = mongoose.createConnection('ongodb+srv://admin:admin101@largeassignment2.vi6pu.mongodb.net/cactusHeaven', {
-    useNewUrlParser: true
+const connection = mongoose.createConnection('mongodb+srv://admin:admin101@largeassignment2.vi6pu.mongodb.net/cactusHeaven', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
 module.exports = {
--- order_service/index.js
+++ order_service/index.js
@@ -39,10 +39,35 @@ const createChannel = connection => new Promise((resolve, reject) => {
 });
 
 // TODO ADD TO DB
-const orderToDB = async (order) => {
+const orderToDB = async (data) => {
+    const order = JSON.parse(data);
+    var totalPrice = 0;
 
+    order.items.forEach(item => {
+        totalPrice += item.quantity * item.unitPrice;
+    });
 
-    return order;
+    const newOrder = {
+        customerEmail: order.email,
+        totalPrice: totalPrice,
+        orderDate: new Date()
+    }
+
+    const createdOrder = await Order.create(newOrder);
+
+    order.items.forEach(item => {
+        const newOrderItem = {
+            description: item.description,
+            quantity: item.quantity,
+            unitPrice: item.unitPrice,
+            rowPrice: item.quantity * item.unitPrice,
+            orderId: createdOrder._id
+        }
+
+        OrderItem.create(newOrderItem);
+    });
+
+    return createdOrder;
 }
 
 (async () => {
@@ -55,9 +80,9 @@ const orderToDB = async (order) => {
 
     console.log(`[x] Taking in messages from: ${orderQueue}`);
 
-    channel.consume(orderQueue, data => {
+    channel.consume(orderQueue, async data => {
         console.log(data.content.toString());
-        const dataJson = JSON.parse(data.content.toString());
+        const dataJson = data.content.toString();
         await orderToDB(dataJson);
         channel.publish(order, output, Buffer.from(dataJson));
 
