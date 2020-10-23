189d3bd948568fff3f1c9392b25318df1e903d69
alexandradilja <alexandrab18@ru.is>
2020-10-23 12:05:29 +0000

    orderservice in progress

--- order_service/index.js
+++ order_service/index.js
@@ -1,1 +1,66 @@
-// TODO: Implement the order service
+const amqp = require('amqplib/callback_api');
+const Order = require('./data/db').Order;
+const OrderItem = require('./data/db').OrderItem;
+
+const messageBrokerInfo = {
+    exchanges: {
+        order: 'order_exchange'
+    },
+    queues: {
+        orderQueue: 'order_queue'
+    },
+    routingKeys: {
+        input: 'create_order',
+        output: 'send_email'
+    }
+}
+
+const createMessageBrokerConnection = () => new Promise((resolve, reject) => {
+    amqp.connect('amqp://localhost', (err, conn) => {
+        if (err) { reject(err); }
+        resolve(conn);
+    });
+});
+
+const configureMessageBroker = channel => {
+    const { exchanges, queues, routingKeys } = messageBrokerInfo;
+
+    channel.assertExchange(exchanges.order, 'direct', { durable: true });
+    channel.assertQueue(queues.orderQueue, { durable: true });
+    channel.bindQueue(queues.orderQueue, exchanges.order, routingKeys.input);
+}
+
+const createChannel = connection => new Promise((resolve, reject) => {
+    connection.createChannel((err, channel) => {
+        if (err) { reject(err); }
+        configureMessageBroker(channel);
+        resolve(channel);
+    });
+});
+
+// TODO ADD TO DB
+const orderToDB = async (order) => {
+
+
+    return order;
+}
+
+(async () => {
+    const connection = await createMessageBrokerConnection();
+    const channel = await createChannel(connection);
+
+    const { order } = messageBrokerInfo.exchanges;
+    const { orderQueue } = messageBrokerInfo.queues;
+    const { output } = messageBrokerInfo.routingKeys;
+
+    console.log(`[x] Taking in messages from: ${orderQueue}`);
+
+    channel.consume(orderQueue, data => {
+        console.log(data.content.toString());
+        const dataJson = JSON.parse(data.content.toString());
+        await orderToDB(dataJson);
+        channel.publish(order, output, Buffer.from(dataJson));
+
+        console.log(`[x] Recived: ${JSON.stringify(dataJson)}`);
+    }, { noAck: true });
+})().catch(e => console.error(e));
